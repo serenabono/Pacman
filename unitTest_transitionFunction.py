d3b68ec4e26a935391215baa8e0985e17f63e1a1
Chloe Loughridge <cloughridge@college.harvard.edu>
2022-06-16 11:48:46 -1000

    chloe did nothing meaningful here

--- unitTest_transitionFunction.py
+++ unitTest_transitionFunction.py
@@ -2,51 +2,6 @@ from transitionFunction import *
 from pacman import *
 import pprint
 
-def runTransitionTest( layout, pacman, ghosts, display, numGames, record, numTraining = 0, catchExceptions=False, timeout=30 ):
-    import __main__
-    __main__.__dict__['_display'] = display
-
-    rules = ClassicGameRules(timeout)
-    games = []
-
-    for i in range( numGames ): # we only need to run the probability calculation once I think per layout
-        print(i)
-        beQuiet = i < numTraining
-        if beQuiet:
-                # Suppress output and graphics
-            import textDisplay
-            gameDisplay = textDisplay.NullGraphics()
-            rules.quiet = True
-        else:
-            gameDisplay = display
-            rules.quiet = False
-        game = rules.newGame( layout, pacman, ghosts, gameDisplay, beQuiet, catchExceptions)
-        
-        #define transition function
-        tree = TransitionFunctionTree(game)
-        tree.computeProbabilities()
-        print(tree.probabilities)
-
-        game.run(i, numGames)
-        if not beQuiet: games.append(game)
-
-        if record:
-            import time, cPickle
-            fname = ('recorded-game-%d' % (i + 1)) +  '-'.join([str(t) for t in time.localtime()[1:6]])
-            f = file(fname, 'w')
-            components = {'layout': layout, 'actions': game.moveHistory}
-            cPickle.dump(components, f)
-            f.close()
-
-    if (numGames-numTraining) > 0:
-        scores = [game.state.getScore() for game in games]
-        wins = [game.state.isWin() for game in games]
-        winRate = wins.count(True)/ float(len(wins))
-        print 'Average Score:', sum(scores) / float(len(scores))
-        print 'Scores:       ', ', '.join([str(score) for score in scores])
-        print 'Win Rate:      %d/%d (%.2f)' % (wins.count(True), len(wins), winRate)
-        print 'Record:       ', ', '.join([ ['Loss', 'Win'][int(w)] for w in wins])
-
 def constructTransitionTest( layout, pacman, ghosts, display, numGames, record, numTraining = 0, catchExceptions=False, timeout=30 ):
     import __main__
     __main__.__dict__['_display'] = display
@@ -67,13 +22,17 @@ def constructTransitionTest( layout, pacman, ghosts, display, numGames, record, 
         #define transition function
         tree = TransitionFunctionTree(game)
         tree.computeProbabilities()
-        tmat = tree.transitionMatrix
-        #tree.printSlicesOfTransitionMatrix()
+        tmat = tree.transitionMatrixDic
+        tree.printSlicesOfTransitionMatrix(0)
+
+        print "the transition matrix dictionary is: "
+        print ("length: %d" % len(tmat))
+        pprint.pprint(tmat)
 
         # confirm the size of the probability tree
         # manual check: there are 
-        print "the transition matrix size is:"
-        print(tmat.shape)
+        # print "the transition matrix size is:"
+        # print(tmat.shape)
         # print "the transition matrix: "
         # for a in tree.actions:
         #     print("slice for action " + a + ":")
