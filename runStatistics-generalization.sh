3132960540f99b0755911bfa45f3860af0fdca62
serenabono <seb300@login04.o2.rc.hms.harvard.edu>
2023-06-20 08:14:30 -0400

    mod generalization bash scrièt

--- runStatistics-generalization.sh
+++ runStatistics-generalization.sh
@@ -39,6 +39,6 @@ echo '''{"epochs":'$epochs',"trained_agents":'$training_agents',"n_training_step
 agentprop='{"test":{"pacman":{},"ghosts":'$trainingenv_ghostarg',"perturb":'$trainingenv_perturb'}}'
 
 folder="generalization_${layout}_${agent}_${trainingenv_ghost_name}_${trainingenv_ghost_args}_${trainingenv_noise_args}}"
-outputname=''''$folder'/saved_agent_'$layout'_'$agent'_'$trainingenv_ghost_name'_'$trainingenv_ghost_args'_'$trainingenv_noise_args'_'$training_agents'-'$DATE'-train'''
+outputname=''''$folder'/saved_agent_'$layout'_'$agent'_'$trainingenv_ghost_name'_'$trainingenv_ghost_args'_'$trainingenv_noise_args'_'$training_agents'-'$RANDOM'-'$DATE'-train'''
 
 python statistics.py -q -m g -p $agent -a $agentprop -l $layout -s '''{"epochs":'$epochs',"trained_agents":'$training_agents',"n_training_steps":'$n_training_steps',"n_testing_steps":'$n_testing_steps',"record_range":'$record_range',"run_untill":'$run_untill',"timeout":30}''' -o  $outputname
